c0bbc0a6df9dc3381e29618565d184f5539397f0
Genjosh <41222199+Genjosh@users.noreply.github.com>
2018-09-12 00:54:54 -0600

    second push

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friendsList = [
+{   
+    name:"Josh",
+    photo: "https://scontent.fapa1-2.fna.fbcdn.net/v/t1.0-9/30595061_2494166253942603_5170899616589152256_n.jpg?_nc_cat=0&oh=4c711eb807432bedf504de168a01fd03&oe=5C322593",
+    answers: [1,4,3,5,3,6]
+    
+
+    }
+];
